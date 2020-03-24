d4f1615a00cdf8638585bfb4dc6520b9f82e374d
Keith Pickford <Keith@FunctionZero.com>
2020-03-24 15:53:05 +0000

    License. Copyright.

--- FunctionZero.CommandZero/Commanding/CommandBuilder.cs
+++ FunctionZero.CommandZero/Commanding/CommandBuilder.cs
@@ -1,7 +1,7 @@
 ﻿/*
 MIT License
 
-Copyright(c) 2016 - 2020 Function Zero Ltd
+Copyright(c) 2019 - 2020 Function Zero Ltd
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -24,7 +24,6 @@ SOFTWARE.
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FunctionZero.CommandZero
